923a3f4ad15074dac90ea85b51e68a9356518a9e
Markus Müller <markus_mueller@de.ibm.com>
2021-09-27 14:20:56 +0200

    use AnomalyScorer

--- kafka-faust-ml/hit_counter.py
+++ kafka-faust-ml/hit_counter.py
@@ -1,3 +1,4 @@
+import logging
 import faust
 import json
 from typing import Set
@@ -6,17 +7,35 @@ from faust.types import AppT, TP
 import numpy as np
 import scipy as sp
 from scipy import stats
+import pandas as pd
 
+from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, func
+from iotfunctions import base
+from iotfunctions import bif
+from iotfunctions import entity
+from iotfunctions import metadata
+from iotfunctions.metadata import EntityType
+from iotfunctions.db import Database
+from iotfunctions.dbtables import FileModelStore
+from iotfunctions.enginelog import EngineLogging
+from mmfunctions import anomaly
 
 app = faust.App('hit_counter',broker="kafka://10.152.183.177:9092")
 
 
 class hitCount(faust.Record,validation=True):
-    hits: int
+    temperature: float
     maxval: float
-    timestamp: float
-    userId: str
+    timestamp: str
+    assetId: str
 
+
+class DatabaseDummy:
+    tenant_id = '###_IBM_###'
+    db_type = 'db2'
+    model_store = FileModelStore()
+    def _init(self):
+        return
 
 hit_topic = app.topic("hit_count", partitions=1, value_type=hitCount)
 count_topic = app.topic('count_topic', internal=True, partitions=1, value_type=hitCount)
@@ -41,27 +60,61 @@ async def count_hits(hits):
     async for hit in hits:
         print(f"Data received is {hit}")
 
+        #<hitCount: temperature=25.3, maxval=0.0, timestamp='1632738034.6980035', assetId='15'>
+
         # get old value from table
         descriptor = None
         try:
-            desc = hits_table[str(hit.userId)]
+            desc = hits_table[str(hit.assetId)]
             print(desc)
             descriptor = json.loads(desc)
         except Exception as e:
             print(e)
             descriptor = {}
-            descriptor[hit.userId] = []
+            descriptor[hit.assetId] = []
 
         # append new value
-        descriptor[hit.userId].append(hit.hits)
-        print (descriptor[hit.userId])
-        if len(descriptor[hit.userId]) > 20:
-            a = np.amax(np.abs(sp.stats.zscore(descriptor[hit.userId])))
+        descriptor[hit.assetId].append({'temperature': hit.temperature, 'timestamp': hit.timestamp})
+
+        if len(descriptor[hit.assetId]) > 60:
+
+            # turn window into proper input for anomaly scoring
+            df = pd.DataFrame.from_records(descriptor[hit.assetId])
+            df['entity'] = hit.assetId
+            df['timestamp'] = pd.to_datetime(df['timestamp'])
+            df.set_index(['entity','timestamp'], inplace=True)
+
+            print('Dataframe columns', df.columns)
+            print('describe ', df.describe())
+
+            # prep
+            db_schema=None
+            db = DatabaseDummy()
+            print (db.tenant_id)
+
+            # call Spectral
+            jobsettings = { 'db': db, '_db_schema': 'public', 'save_trace_to_file' : True}
+
+            EngineLogging.configure_console_logging(logging.DEBUG)
+
+            #spsi = anomaly.SpectralAnomalyScoreExt('temperature', 12, ['spectral', 'spectralinv'])
+            spsi = anomaly.AnomalyScorer('temperature', 12, ['spectral'])
+            et = spsi._build_entity_type(columns = [Column('temperature',Float())], **jobsettings)
+            spsi._entity_type = et
+            df = spsi.execute(df=df)
+            EngineLogging.configure_console_logging(logging.WARNING)
+
+            print(df.columns)
+
+            # return something back - could be a json encoded df
+            a = np.amax(np.abs(df['spectral']))
             hit.maxval = max(hit.maxval, a)
+
             print('Send ', hit.maxval)
             await count_topic.send(value=hit)
-            descriptor[hit.userId] = []
-        hits_table[str(hit.userId)] = json.dumps(descriptor)
+            descriptor[hit.assetId] = []
+
+        hits_table[str(hit.assetId)] = json.dumps(descriptor)
 
 
 
@@ -70,6 +123,6 @@ async def increment_count(counts):
     print ('Received ', counts)
     async for count in counts:
         print(f"Count in internal topic is {count}")
-        count_table[str(count.userId)]+=1
-        print(f'{str(count.userId)} has now been seen {count_table[str(count.userId)]} times')
+        count_table[str(count.assetId)]+=1
+        print(f'{str(count.assetId)} has now been seen {count_table[str(count.assetId)]} times')
 
--- kafka-faust-ml/kafkaproducer.py
+++ kafka-faust-ml/kafkaproducer.py
@@ -1,23 +1,29 @@
 from confluent_kafka import Producer
+import argparse
 import uuid
 import json
 import time
 import random
+import pandas as pd
 
 
-def generate_random_time_series_data(count=0):
-    new_message = {"hits":random.randint(10,100),"maxval": 0., "timestamp":time.time(),"userId":random.randint(15,15)}
-    return count+1,new_message
+def generate_random_time_series_data(count, act_temp, act_time):
+    current_temp = act_temp + random.gauss(0,1)
+    current_time = act_time + pd.Timedelta("5 seconds")
+    new_message = {"temperature":current_temp, "maxval": 0., "timestamp": str(current_time), "assetId":random.randint(15,15)}
+    return count+1, current_temp, current_time, new_message
 
 
-def kafka_producer():
+def kafka_producer(totnr):
     bootstrap_servers = "10.152.183.177:9092"
     topic = "hit_count"
     p = Producer({'bootstrap.servers': bootstrap_servers})
-    total = 1
+    act_temp = 24.0
+    act_time = pd.Timestamp.now()
+    total = totnr
     count =0
     while total:
-        count,base_message = generate_random_time_series_data(count)
+        count, act_temp, act_time, base_message = generate_random_time_series_data(count, act_temp, act_time)
         total-=1
 
         record_key = str(uuid.uuid4())
@@ -30,4 +36,7 @@ def kafka_producer():
     print('we\'ve sent {count} messages to {brokers}'.format(count=count, brokers=bootstrap_servers))
 
 if __name__ == "__main__":
-  kafka_producer()
+    parser = argparse.ArgumentParser(description='Get total nr of messages.')
+    parser.add_argument('total', metavar='N', type=int, default=1, nargs='+', help='number of messages')
+    args = parser.parse_args()
+    kafka_producer(args.total[0])
